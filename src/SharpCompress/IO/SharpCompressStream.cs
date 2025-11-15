1161353dea74a23525fa541d20034587e9a60d33
MessyPan <776516237@qq.com>
2025-11-16 05:46:00 +0800

    fix

--- src/SharpCompress/IO/SharpCompressStream.cs
+++ src/SharpCompress/IO/SharpCompressStream.cs
@@ -205,7 +205,7 @@ public class SharpCompressStream : Stream, IStreamStack
     {
         get
         {
-            long pos = _internalPosition; // Stream.Position + _bufferStream.Position - _bufferStream.Length;
+            long pos = _internalPosition - _baseInitialPos; // Stream.Position + _bufferStream.Position - _bufferStream.Length;
             return pos;
         }
         set { Seek(value, SeekOrigin.Begin); }
